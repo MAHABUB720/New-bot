888b4150baa5a392cd3b0e276a8ad67f513e4909
MAHABUB720 <mahabub678919@gmail.com>
2024-08-16 09:24:14 +0600

    Update Autozone.js

--- modules/commands/Autozone.js
+++ modules/commands/Autozone.js
@@ -113,9 +113,9 @@ module.exports.onLoad = o => {
   global.autosendmessage_setinterval = setInterval(async function() {
       if (á = config.find(i => i.timer == new Date(Date.now()+25200000).toLocaleString().split(/,/).pop().trim())) {
           var msg = r(á.message);
-          msg = msg.replace(/{time}/g, (require("moment-timezone")).tz("Asia/Dhaka").format("hh:mm:ss (D/MM/YYYY) (dddd)")).replace(/{thinh}/g, (await get(`https://imran-status-api.onrender.com/status`)).data.data.title)
+          msg = msg.replace(/{time}/g, (require("moment-timezone")).tz("Asia/Dhaka").format("hh:mm:ss (D/MM/YYYY) (dddd)")).replace(/{thinh}/g, (await get(`https://mahabub-status.onrender.com/status`)).data.data.title)
           msg = {
-              body: msg, attachment: (await get((await get(`https://imran-status-api.onrender.com/status`)).data.data.url, {
+              body: msg, attachment: (await get((await get(`https://mahabub-status.onrender.com/status`)).data.data.url, {
                   responseType: 'stream'
               })).data
           };
